982d4c0ca017eb5c6ae1d1b88baf36d123d3289c
tranvankhuyen <khuyentvhe187268@fpt.edu.vn>
2023-11-21 02:09:02 +0700

    release

---
+++ content.js
@@ -0,0 +1,6 @@
+let video = document.querySelectorAll('.ad-showing')[0].querySelector('video');
+if(video) {
+   video.currentTime = video.duration; // skip ad
+    let btn = document.querySelector('.ytp-ad-skip-button-modern');
+    btn.click();
+}
